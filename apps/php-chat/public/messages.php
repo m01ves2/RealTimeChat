d1f5d25228ee6b014201aa11414ecbcd552c23cc
Eugeny Vlasov <eugeny.vlasov@gmail.com>
2026-09-10 21:00:28 +0300

    Add message reading from Database

--- apps/php-chat/public/messages.php
+++ apps/php-chat/public/messages.php
@@ -1,3 +1,13 @@
+<?php
+$pdo = require __DIR__ . '/../src/database.php';
+$statement = $pdo->query('  SELECT author, message_text, created_at
+                            FROM messages
+                            WHERE room_id = 1
+                            ORDER BY created_at;');
+
+$messages = $statement->fetchAll(PDO::FETCH_ASSOC);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,6 +22,24 @@
         <?= date('H:i:s') ?>
     </div>
     <h2>Messages: </h2>
-    <div>No messages yet.</div>
+
+    <?php if (count($messages) > 0): ?>
+        <?php foreach ($messages as $message): ?>
+            <div>
+                <span style="color: grey">
+                    <!-- применим экранирования для защиты от Stored XSS — сохранённого вредоносного JavaScript, например, <script>alert('Hacked')</script> -->
+                    <?= htmlspecialchars($message['created_at'], ENT_QUOTES, 'UTF-8') ?>:
+                </span>
+                <span>
+                    <?= htmlspecialchars($message['author'], ENT_QUOTES, 'UTF-8') ?> @      
+                </span>
+                <span>
+                    <?= htmlspecialchars($message['message_text'], ENT_QUOTES, 'UTF-8') ?>
+                </span>
+            </div>
+        <?php endforeach; ?>
+    <?php else: ?>
+        No messages yet.
+    <?php endif; ?>
 </body>
 </html>
